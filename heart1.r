6226c84d23cb26ef8234efe5e3d4a68208137425
MA Jian <majian1979@qq.com>
2022-05-07 16:23:58 +0800

    Update heart1.r

--- heart1.r
+++ heart1.r
@@ -45,10 +45,12 @@ ce58 = ce58 / l
 ce58[c(1,2,58)] = min(ce58)
 
 # Other Independence Measures
+ktau58 = rep(0,n) # Kendall's tau
 dcor58 = rep(0,n) # Distance Correlation
 dhsic58 = rep(0,n)  # Hilbert-Schmidt Independence Criterion
 hhg58 = rep(0,n)  # Heller-Heller-Gorfine Tests
-ind58 = rep(0,n)  # Hoeffding's D test or Bergsma-Dassios T* sign covariance
+ind58 = rep(0,n)  # Hoeffding's D test 
+bdtau58 = rep(0,n) # Bergsma-Dassios T* sign covariance
 ball58 = rep(0,n) # Ball correlation
 qad58 = rep(0,n) # Quantification of Asymmetric Dependence
 bet58 = rep(0,n) # Binary Expansion Testing
@@ -58,14 +60,15 @@ subcop58 = rep(0,n) #supremum dependence
 mdm58 = rep(0,n) # mutual independence measure
 codec58 = rep(0,n) # dependence coefficient
 for (i in 1:n){
+  ktau58[i] = cor(heart1[,c(i,58)], method = "kendall")[1,2]
   dcor58[i] = dcor(heart1[,i],heart1[,58])
   dhsic58[i] = dhsic(heart1[,i],heart1[,58])$dHSIC
   Dx = as.matrix(dist((heart1[,i]), diag = TRUE, upper = TRUE))
   Dy = as.matrix(dist((heart1[,58]), diag = TRUE, upper = TRUE))
-  hhg58[i] = hhg.test(Dx,Dy, nr.perm = 1000)
+  hhg58[i] = hhg.test(Dx,Dy, nr.perm = 1000)$sum.chisq
   ind58[i] = hoeffding.D.test(heart1[,i],heart1[,58])$Dn
   #ind58[i] = hoeffding.refined.test(heart1[,i],heart1[,58])$Rn
-  #ind58[i] = tau.star.test(heart1[,i],heart1[,58])$Tn
+  bdtau58[i] = tau.star.test(heart1[,i],heart1[,58])$Tn
   ball58[i] = bcor(heart1[,i],heart1[,58])
   qad58[i] = qad(heart1[,i],heart1[,58])$`q(X,Y)`
   bet58[i] = BEAST(heart1[,c(i,58)], 3, index = list(1,2))$BEAST.Statistic
@@ -75,11 +78,13 @@ for (i in 1:n){
   mdm58[i] = mdm_test(heart1[,c(i,58)])$stat
   codec58[i] = codec(heart1[,i],heart1[,58])
 }
+ktau50[c(1,2,58)] = 0
 dcor58[c(1,2,58)] = 0
 dhsic58[c(1,2,58)] = 0
 hhg58 = unlist(hhg58)
 hhg58[c(1,2,58)] = 0
 ind58[c(1,2,58)] = 0
+bdtau58[c(1,2,58)] = 0
 ball58[c(1,2,58)] = 0
 qad58[c(1,2,58)] = 0
 bet58[c(1,2,58)] = 0
@@ -97,6 +102,13 @@ lines(ce58)
 axis(side = 1, at = c(seq(1,75, by = 5)), labels = c(seq(1,75, by = 5)))
 th16a = rep(ce58[16],75)
 lines(th16a, col = "red")
+# Kendall's tau
+x11(width = 10, height = 5)
+plot(ktau58, xlab = "Variable", ylab = "Kendall's tau", xaxt = 'n')
+lines(ktau58)
+axis(side = 1, at = c(seq(1,75, by = 5)), labels = c(seq(1,75, by = 5)))
+th16a = rep(ktau58[16],75)
+lines(th16a, col = "red")
 # dcor
 x11(width = 10, height = 5)
 plot(dcor58, xlab = "Variable", ylab = "dCor", xaxt = 'n')
@@ -113,7 +125,7 @@ th16c = rep(dhsic58[16],75)
 lines(th16c, col = "red")
 # hhg
 x11(width = 10, height = 5)
-plot(hhg58, xlab = "Variable", ylab = "HHG", xaxt = 'n')
+plot(hhg58, xlab = "Variable", ylab = "HHG.chisq", xaxt = 'n')
 lines(hhg58)
 axis(side = 1, at = c(seq(1,75, by = 5)), labels = c(seq(1,75, by = 5)))
 th16d = rep(hhg58[16],75)
@@ -124,6 +136,13 @@ plot(ind58, xlab = "Variable", ylab = "Hoeffding", xaxt = 'n')
 lines(ind58)
 axis(side = 1, at = c(seq(1,75, by = 5)), labels = c(seq(1,75, by = 5)))
 th16e = rep(ind58[16],75)
+lines(th16e, col = "red")
+# BD tau
+x11(width = 10, height = 5)
+plot(bdtau58, xlab = "Variable", ylab = "BD tau", xaxt = 'n')
+lines(bdtau58)
+axis(side = 1, at = c(seq(1,75, by = 5)), labels = c(seq(1,75, by = 5)))
+th16e = rep(bdtau58[16],75)
 lines(th16e, col = "red")
 # ball
 x11(width = 10, height = 5)
